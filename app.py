654f59295fb85f7a015dafd4347a0cabe5ed96e1
VIVEK KUMAR <32438255+vivekvvk143@users.noreply.github.com>
2020-05-06 20:49:09 +0530

    Files added

---
+++ app.py
@@ -0,0 +1,143 @@
+
+from flask import Flask ,render_template , request, jsonify
+from flask_uploads import UploadSet , configure_uploads, IMAGES
+from attention8kfinal import main
+import os
+from flask_cors import CORS,cross_origin
+from flask_mail import Mail, Message 
+import socket
+
+
+
+app = Flask(__name__)
+mail = Mail(app)
+CORS(app)
+
+photos = UploadSet('photos', IMAGES)
+
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
+configure_uploads(app , photos)
+
+#**********************************************************************************************************************************#
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+#**********************************************************************************************************************************#
+email = os.environ.get("Email")
+password = os.environ.get("Email_pass")
+
+# configuration of mail 
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = email
+app.config['MAIL_PASSWORD'] = password
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+mail = Mail(app) 
+
+@app.route('/SubmitDetails',methods=['POST','GET'])
+def submitDetails():
+	if request.method == 'POST':
+		json =request.get_json()
+		recipient = json['email']
+		message = 'Hey team, \n'+ json['name']+' has tried to contact you... with the following number :- '+json['phone']+'.\n He/She has the following message for you :- \n'+json['message']
+		'''server = smtplib.SMTP("smtp.gmail.com",587)
+		server.starttls()
+		server.login('vivekvvk143@gmail.com','Vivek@143')
+		server.sendmail("vivekvvk143@gmail.com",'vivekvvk143@gmail.com',message)'''
+		try:
+			msg = Message(
+					'Someone in contact window...',
+					sender=recipient,
+					recipients= ['vivekvvk143@gmail.com']
+					)
+			msg.body = message
+			mail.send(msg)
+			sendResponse(json['name'],json['email'])
+			return {"status":'success'}
+
+		except socket.gaierror:
+			return {"status":'failure'}
+
+	return "Failed"
+
+
+def sendResponse(name,emailId):
+	message = 'Hey '+name+', Thank you for feedback. We will get back to you soon shortly.'
+	msg = Message(
+				  'Response from Image-chronicle',
+				  sender='vivekvvk143@gmail.com',
+				  recipients=[emailId]
+				  )
+	msg.body = message
+	mail.send(msg)
+	return True
+
+
+#****************************************************************************************************************************************#
+
+
+@app.route('/newsletterSubscription',methods=['POST'])
+def newsletterSubscription():
+	if request.method =='POST':
+		json = request.get_json()
+		message = 'Thank you for subscribing our newsletter.You will now be notified whenever we post something new.'
+		try:
+			msg = Message(
+					  'Image-chronicle newsletter subscription',
+					  sender='vivekvvk143@gmail.com',
+					  recipients=[json['email']]
+					  )
+			msg.body = message
+			mail.send(msg)
+			return {"status":'success'}
+		except socket.gaierror:
+			return {"status":'failure'}
+			
+	return "Failed"
+
+
+		
+
+#*****************************************************************************************************************************************#
+
+@app.route('/prediction',methods=['POST','GET'])
+def predictions():
+	''' This method takes the image file from the front-end 
+		and then forwards this file to the 
+		ML Model for further processing...
+	'''
+	if request.method == 'POST':
+		#print(request.files)
+		if request.files['photo']:
+			path = photos.save(request.files['photo'])
+			path ='.\\static\\img\\'+path
+			caption = main(path)
+			os.remove(path);
+			return caption
+		else:
+			return "No file"
+	return "Failed"
+
+
+
+#***************************************************************************************************************************************#
+
+@app.route('/prediction1',methods = ['POST','GET'])
+def prediction1():
+	'''This function handles the local images of the app and does the same functionality as the above function...'''
+	if request.method == "POST":
+		json = request.get_json()
+		path1 = json["path"]
+		caption = main("."+path1)
+		return caption
+
+
+#************************************************************************************************************************************#
+
+if __name__ == "__main__":
+	'''Run this function with host=0.0.0.0 to run this server for all the devices on your network...'''
+	app.run(host='0.0.0.0',debug=True,threaded=True)
---
+++ static/react/precache-manifest.27dbd5488bae69fcbd5058cf9029ac9b.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff45413d22424337407ad4edb4d1315",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "08610908a46a3962f1b4",
+    "url": "/static/react/css/2.9692f31f.chunk.css"
+  },
+  {
+    "revision": "e44b94d8ff0e95c2730a",
+    "url": "/static/react/css/main.33a8d5c0.chunk.css"
+  },
+  {
+    "revision": "08610908a46a3962f1b4",
+    "url": "/static/react/js/2.6f212dbf.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/react/js/2.6f212dbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e44b94d8ff0e95c2730a",
+    "url": "/static/react/js/main.0bc14aae.chunk.js"
+  },
+  {
+    "revision": "78dfcfd8a2301a1010ad",
+    "url": "/static/react/js/runtime-main.ae9a3691.js"
+  },
+  {
+    "revision": "45cf3b128ba4fa91c8bb3c8a297d41d1",
+    "url": "/static/react/media/1.45cf3b12.jpg"
+  },
+  {
+    "revision": "18b74168ccffcf9f566c82029e3710c2",
+    "url": "/static/react/media/2.18b74168.jpg"
+  },
+  {
+    "revision": "3fdbb551646ab0bea396b83b72db1c07",
+    "url": "/static/react/media/3.3fdbb551.jpg"
+  },
+  {
+    "revision": "53d551901f7913a5765e679eef1e929d",
+    "url": "/static/react/media/4.53d55190.jpg"
+  },
+  {
+    "revision": "5445af9abeef974f702781b9093f5ccf",
+    "url": "/static/react/media/5.5445af9a.jpg"
+  },
+  {
+    "revision": "d28f3c512d9f2016f81095ca7b87605e",
+    "url": "/static/react/media/6.d28f3c51.jpg"
+  },
+  {
+    "revision": "26598828b6fdeef8637dd3096e1f18a9",
+    "url": "/static/react/media/7.26598828.jpg"
+  },
+  {
+    "revision": "607c03aa8b6789c8bce5a7146c5df017",
+    "url": "/static/react/media/8.607c03aa.jpg"
+  },
+  {
+    "revision": "9e4626bbda187d7babd935ab0f10d5a5",
+    "url": "/static/react/media/Abhishek.9e4626bb.jpeg"
+  },
+  {
+    "revision": "8dfc3fcffaf49d3c44148aac10846096",
+    "url": "/static/react/media/Adarsh.8dfc3fcf.jpg"
+  },
+  {
+    "revision": "85f7ee39919cac6fa18dac7c1a66c922",
+    "url": "/static/react/media/Vivek.85f7ee39.png"
+  },
+  {
+    "revision": "d675e343d7ef6d451c99754eb0ff46d2",
+    "url": "/static/react/media/about_back.d675e343.jpg"
+  },
+  {
+    "revision": "349353b29840087e0ea93f5f2f1faeeb",
+    "url": "/static/react/media/back.349353b2.jpg"
+  },
+  {
+    "revision": "efb69a7a7a6620266fb9c6038b03c960",
+    "url": "/static/react/media/dark_back.efb69a7a.jpeg"
+  },
+  {
+    "revision": "c7b7abe403dda38cef36ac24c72ab902",
+    "url": "/static/react/media/project_back.c7b7abe4.png"
+  },
+  {
+    "revision": "cff40dbb8eac83e4e9f1e6b8a0272adb",
+    "url": "/static/react/media/surf.cff40dbb.jpg"
+  },
+  {
+    "revision": "9ee75cb182ee9d8b1c6e69407fc2c357",
+    "url": "/static/react/media/works.9ee75cb1.jpg"
+  }
+]);
